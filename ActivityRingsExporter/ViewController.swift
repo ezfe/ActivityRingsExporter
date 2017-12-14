c9633808a6d73bdc5b5faadf3a22b8caa46f20c6
Ezekiel Elin <ezekielelin@me.com>
2017-12-14 18:06:47 -0500

    Initial Commit

---
+++ ActivityRingsExporter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ActivityRingsExporter
+//
+//  Created by Ezekiel Elin on 12/14/17.
+//  Copyright © 2017 Ezekiel Elin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
